c9e40b2500b33c8acc790936da73ed0d34446a59
Ampgh Tambad <amoghtambad@gmail.com>
2024-02-14 19:15:16 -0700

    First commit

---
+++ Project 1/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import requests
+import pandas as pd
+import warnings
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+app = Flask(__name__)
+
+classResDict = {}
+
+classRes = pd.read_csv('Classification Results.csv')
+
+for ele in classRes.iterrows():
+    classResDict[ele[1][0]] = ele[1][1]
+
+
+@app.route("/", methods=["GET", "POST"])
+def test_file_upload():
+    if request.method == "POST":
+        form = request.files['inputFile']
+        filename = form.filename.split('.')[0]
+        return "{}:{}".format(filename, classResDict[filename])
+    else:
+        return "Server is running!"
+    
+if __name__ == "__main__":
+    app.run(debug=False)
