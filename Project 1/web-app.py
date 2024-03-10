2a6797a6088a53fa58906eda824bfaf4e1992074
Ampgh Tambad <amoghtambad@gmail.com>
2024-03-10 12:44:09 -0700

    Getting web-tier to work independently first

--- Project 1/web-app.py
+++ Project 1/web-app.py
@@ -7,22 +7,22 @@ import requests
 warnings.simplefilter(action='ignore', category=FutureWarning)
 app = Flask(__name__)
 
-# classResDict = {}
+classResDict = {}
 
-# classRes = pd.read_csv('Classification Results.csv')
+classRes = pd.read_csv('Classification Results.csv')
 
-# for ele in classRes.iterrows():
-#     classResDict[ele[1][0]] = ele[1][1]
+for ele in classRes.iterrows():
+    classResDict[ele[1][0]] = ele[1][1]
 
 
 @app.route("/", methods=["GET", "POST"])
 def file_upload():
     if request.method == "POST":
         form = request.files['inputFile']
-        # filename = form.filename.split('.')[0]
+        filename = form.filename.split('.')[0]
         file = {"inputFile":open(form, 'rb')}
-        response = requests.post(url="54.242.102.215", files=file)
-        # return "{}:{}".format(filename, classResDict[filename])
+        # response = requests.post(url="54.242.102.215", files=file)
+        return "{}:{}".format(filename, classResDict[filename])
     else:
         return "Server is running!"
     
